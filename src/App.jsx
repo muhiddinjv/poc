3e7d388c7b55e247fa90df1ceaa9baecf2dd9048
Muhiddin <muhiddinjv@gmail.com>
2024-08-28 18:03:50 +0500

    remove auth

--- src/App.jsx
+++ src/App.jsx
@@ -4,48 +4,18 @@ import NoPage from "./pages/NoPage";
 import Words from "./words/Words";
 import Quiz from "./games/quiz/Quiz";
 import Story from "./pages/Story";
-import SignUp from "./pages/SignUp";
-import SignIn from "./pages/SignIn";
-import PrivateRoute from "./components/PrivateRoute";
+// import SignUp from "./pages/SignUp";
+// import SignIn from "./pages/SignIn";
+// import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route
-          index
-          element={
-            <PrivateRoute>
-              <Stories />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/words/1"
-          element={
-            <PrivateRoute>
-              <Words />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/games/1"
-          element={
-            <PrivateRoute>
-              <Quiz />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/story/1"
-          element={
-            <PrivateRoute>
-              <Story />
-            </PrivateRoute>
-          }
-        />
+        <Route index element={<Stories />} />
+        <Route path="/words/1" element={<Words />} />
+        <Route path="/games/1" element={<Quiz />} />
+        <Route path="/story/1" element={<Story />} />
         <Route path="*" element={<NoPage />} />
       </Routes>
     </BrowserRouter>
