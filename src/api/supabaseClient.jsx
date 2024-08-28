f30d35d093b25f9ad931ebf370b98a287a3ad0cb
Muhiddin <muhiddinjv@gmail.com>
2024-08-28 18:11:02 +0500

    remove consolelog

--- src/api/supabaseClient.jsx
+++ src/api/supabaseClient.jsx
@@ -2,6 +2,5 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://mdrvdactoucxhhaxjijl.supabase.co';
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
-console.log(supabaseKey, supabaseUrl)
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
