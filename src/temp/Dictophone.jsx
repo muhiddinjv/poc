b3e1d91915761b0d6fa422f5efa6ea237eb7f236
Muhiddin <muhiddinjv@gmail.com>
2024-08-30 17:01:15 +0500

    comment out unused code

--- src/temp/Dictophone.jsx
+++ src/temp/Dictophone.jsx
@@ -5,33 +5,33 @@ import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognitio
 // If not, you can install it by running the following command:
 // npm install assemblyai
 
-import { AssemblyAI } from 'assemblyai';
+// import { AssemblyAI } from 'assemblyai';
 
-const client = new AssemblyAI({
-  apiKey: '478c3155c2384010bdd7ebbfc6b10844',
-});
+// const client = new AssemblyAI({
+//   apiKey: '478c3155c2384010bdd7ebbfc6b10844',
+// });
 
-const FILE_URL = 'https://storage.googleapis.com/aai-web-samples/5_common_sports_injuries.mp3';
+// const FILE_URL = 'https://storage.googleapis.com/aai-web-samples/5_common_sports_injuries.mp3';
 
 // You can also transcribe a local file by passing in a file path
 // const FILE_URL = './path/to/file.mp3';
 
 // Request parameters where auto_highlights has been enabled
-const data = {
-  audio: FILE_URL,
-  auto_highlights: true
-}
+// const data = {
+//   audio: FILE_URL,
+//   auto_highlights: true
+// }
 
-const run = async () => {
-  const transcript = await client.transcripts.transcribe(data);
-  console.log(transcript.text);
+// const run = async () => {
+//   const transcript = await client.transcripts.transcribe(data);
+//   console.log(transcript.text);
 
-  for (let result of transcript.auto_highlights_result.results) {
-    console.log(
-      `Highlight: ${result.text}, Count: ${result.count}, Rank: ${result.rank}`
-    );
-  }
-};
+//   for (let result of transcript.auto_highlights_result.results) {
+//     console.log(
+//       `Highlight: ${result.text}, Count: ${result.count}, Rank: ${result.rank}`
+//     );
+//   }
+// };
 
 // run();
 
