5b5d16d8a4a9a4b8a3c9f9c1a98903269a737fe9
Muhiddin <muhiddinjv@gmail.com>
2024-08-31 23:45:53 +0500

    remove clerk from main

--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,19 @@
-import "regenerator-runtime/runtime";
-import React from "react";
-import "./index.css";
-import App from "./App.jsx";
-import { createRoot } from "react-dom/client";
+import 'regenerator-runtime/runtime'
+import React from 'react'
+import './index.css'
+import App from './App.jsx'
+import {createRoot} from 'react-dom/client'
 import { Analytics } from "@vercel/analytics/react";
-import { ClerkProvider } from "@clerk/clerk-react";
-
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-
-if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key");
-}
 
 let container = null;
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener('DOMContentLoaded', function() {
   if (!container) {
-    container = document.getElementById("root");
+    container = document.getElementById('root');
     const root = createRoot(container);
     root.render(
       <React.StrictMode>
-        <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-          <App />
-          <Analytics />
-        </ClerkProvider>
+        <App/>
+        <Analytics />
       </React.StrictMode>
-    );
-  }
+    )}
 });
