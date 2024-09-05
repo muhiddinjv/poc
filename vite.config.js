fb6188cb2ebc8779dae7b88f1c70c9c451241423
Muhiddin <muhiddinjv@gmail.com>
2024-09-05 08:19:44 +0500

    update install logo

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { VitePWA } from "vite-plugin-pwa";
 // import dotenv from "dotenv";
 // dotenv.config();
 
-let faviconURL = "/img/logo-text-lg.png";
+let faviconURL = "/img/logo-only.png";
 
 export default defineConfig({
   // build: {
